05f490b1cf3b2bb96d59295631ad2271c6777df6
deblocka <fdbxf8ixfn>
2020-05-06 16:34:00 +0200

    feat(ci): adding postman test via preprod serv

--- script/create_db.sql
+++ script/create_db.sql
@@ -35,21 +35,3 @@ CREATE TABLE qsi.module_cours (
 	FOREIGN KEY(cours) REFERENCES cours(title),
 	FOREIGN KEY(module) REFERENCES module(title)
 );
-
-INSERT INTO qsi.parcours VALUES ('MIAGE' , 'Un parcours de test pour tester que ça marche comme il faut');
-
-INSERT INTO qsi.module VALUES ('Licence 3 MIAGE' , 'Un parcours de licence MIAGE pour tester que ça marche comme il faut');
-INSERT INTO qsi.module VALUES ('Master 1 MIAGE' , 'Un parcours de Master 1 MIAGE pour tester que ça marche comme il faut');
-INSERT INTO qsi.module VALUES ('Master 2 MIAGE' , 'Un parcours de Master 2 MIAGE pour tester que ça marche comme il faut');
-
-INSERT INTO qsi.cours VALUES ('QSI' , 'Un cours de Master 2 MIAGE pour tester que ça marche comme il faut', 'https://video.fr');
-INSERT INTO qsi.cours VALUES ('Fouille de données' , 'Un cours de Master 1 MIAGE pour tester que ça marche comme il faut', 'https://video.fr');
-INSERT INTO qsi.cours VALUES ('POO' , 'Un cours de Licence 3 pour tester que ça marche comme il faut', 'https://video.fr');
-
-INSERT INTO qsi.parcours_module VALUES ('MIAGE', 'Licence 3 MIAGE', 1);
-INSERT INTO qsi.parcours_module VALUES ('MIAGE', 'Master 1 MIAGE', 2);
-INSERT INTO qsi.parcours_module VALUES ('MIAGE', 'Master 2 MIAGE', 3);
-
-INSERT INTO qsi.module_cours VALUES ('Licence 3 MIAGE', 'POO');
-INSERT INTO qsi.module_cours VALUES ('Master 1 MIAGE', 'Fouille de données');
-INSERT INTO qsi.module_cours VALUES ('Master 2 MIAGE', 'QSI');
