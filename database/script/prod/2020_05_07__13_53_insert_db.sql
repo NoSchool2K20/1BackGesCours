877cdc6b1cff5dd87460caf40d74867841788998
deblocka <fdbxf8ixfn>
2020-05-07 14:19:36 +0200

    feat(db): adjusting new data

--- database/script/prod/2020_05_07__13_53_insert_db.sql
+++ database/script/prod/2020_05_07__13_53_insert_db.sql
@@ -4,7 +4,7 @@ INSERT INTO qsi.parcours(title, description) VALUES ('Developpeur Fullstack', 'L
 INSERT INTO qsi.module(title, description) VALUES ('Master 1 AC', 'Le Master 1 est la premiere annee pour l''obtention du diplôme, elle donne les connaissances de base sur la cybersecurite.');
 INSERT INTO qsi.module(title, description) VALUES ('Master 2 AC', 'Le Master 2 est la derniere annee pour l''obtention du diplôme, elle consolide les connaissances sur la cybersecurite et se termine par un stage en entreprise.');
 INSERT INTO qsi.module(title, description) VALUES ('Master 1 DF', 'Le Master 1 est la premiere annee pour l''obtention du diplôme, elle approndit les connaissances en developpement et competences humaines.');
-INSERT INTO qsi.module(title, description) VALUES ('Master 1 DF', 'Le Master 2 est la derniere annee pour l''obtention du diplôme, elle consolide les connaissances en developpement et se termine par un stage en entreprise.');
+INSERT INTO qsi.module(title, description) VALUES ('Master 2 DF', 'Le Master 2 est la derniere annee pour l''obtention du diplôme, elle consolide les connaissances en developpement et se termine par un stage en entreprise.');
 
 INSERT INTO qsi.parcours_module(parcours, module, niveau) VALUES ('Analyste Cyberdefense', 'Master 1 AC', 1); 
 INSERT INTO qsi.parcours_module(parcours, module, niveau) VALUES ('Analyste Cyberdefense', 'Master 2 AC', 2); 
