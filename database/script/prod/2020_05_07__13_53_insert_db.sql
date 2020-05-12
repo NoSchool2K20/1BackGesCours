2ae5fe4b4956dc1edcbee91ed314b8a0180eec31
Valentin <valentintartare59@gmail.com>
2020-05-12 15:22:50 +0200

    bdd() : adapt data to model

--- database/script/prod/2020_05_07__13_53_insert_db.sql
+++ database/script/prod/2020_05_07__13_53_insert_db.sql
@@ -11,13 +11,13 @@ INSERT INTO qsi.parcours_module(parcours, module, niveau) VALUES ('Analyste Cybe
 INSERT INTO qsi.parcours_module(parcours, module, niveau) VALUES ('Developpeur Fullstack', 'Master 1 DF', 1);
 INSERT INTO qsi.parcours_module(parcours, module, niveau) VALUES ('Developpeur Fullstack', 'Master 2 DF', 2);
 
-INSERT INTO qsi.cours(title, description, video_url) VALUES ('Blockchain et crypto-monnaie : analyse et mise en oeuvre','Ce cours vise a expliquer les concepts de blockchain et crypto-monnaie, en insistant sur les differences entre ceux-ci.','https://www.youtube.com/embed/lr9sqK2PpnU');
+INSERT INTO qsi.cours(title, description, video_url) VALUES ('Blockchain et crypto-monnaie','Ce cours vise a expliquer les concepts de blockchain et crypto-monnaie, en insistant sur les differences entre ceux-ci.','https://www.youtube.com/embed/lr9sqK2PpnU');
 INSERT INTO qsi.cours(title, description, video_url) VALUES ('Rappels mathematiques','Ce cours vise a rappeler et approfondir les notions mathematiques necessaires a la poursuite du programme.','https://www.youtube.com/embed/F6-qP8vvJYg');
 INSERT INTO qsi.cours(title, description, video_url) VALUES ('Securite des reseaux et des systemes informatiques','Ce cours vise a consolider les connaissances en reseaux et systemes des etudiants.','https://www.youtube.com/embed/jGRkdzEd0PY');
 INSERT INTO qsi.cours(title, description, video_url) VALUES ('Algorithmes et complexite','L''objectif du cours est de preparer les etudiants a repondre aux questions de conception, d''efficacite et de complexite. d''algorithmes.','https://www.youtube.com/embed/x1sFJ_HKEaI');
 INSERT INTO qsi.cours(title, description, video_url) VALUES ('Technologies pour applications connectees','L''UE Technologies pour Applications Connectees (TAC) se concentre sur le developpement des applications connectees a Internet.','https://www.youtube.com/embed/_JNeiGbAgL4');
 
-INSERT INTO qsi.module_cours(module, cours) VALUES ('Master 1 AC', 'Blockchain et crypto-monnaie : analyse et mise en oeuvre');
+INSERT INTO qsi.module_cours(module, cours) VALUES ('Master 1 AC', 'Blockchain et crypto-monnaie');
 INSERT INTO qsi.module_cours(module, cours) VALUES ('Master 1 AC', 'Rappels mathematiques');
 INSERT INTO qsi.module_cours(module, cours) VALUES ('Master 2 AC', 'Securite des reseaux et des systemes informatiques');
 INSERT INTO qsi.module_cours(module, cours) VALUES ('Master 1 DF', 'Algorithmes et complexite');
